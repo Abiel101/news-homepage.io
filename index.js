86efb07a139ef85f4a3e0ee6108f320e67c5c2aa
Abiel Ortega <abielortega90@gmail.com>
2022-12-27 23:22:10 +0000

    Fixed up the top part might do some refactoring a little later

---
+++ index.js
@@ -0,0 +1,10 @@
+let abiel = {
+        hair: "black",
+        eyes: "brown",
+        sayHi: function(){
+                return "Hi I am Abiel!";
+        }
+};
+
+console.log(abiel.hair);
+console.log(abiel.sayHi());
